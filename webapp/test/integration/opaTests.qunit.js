e25a678861caeb5423066c05e217107633ff27ab
David Garcia <david.garcia@convista.com>
2026-05-09 12:54:00 +0000

    Initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["devssummit2026/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"devssummit2026/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
